c1357d3197f97b74fa3428c908f00a4e0004f384
Raj Mandloii <rajmandloi01.01@gmail.com>
2022-12-02 16:12:02 +0530

    footer ui added

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,12 +1,54 @@
 import React from 'react'
+import "./Footer.scss"
+const Footer = () => {
+    return (
+        <div className='footer'>
 
- const Footer = () => {
-  return (
-    <div className='footer'>
+            <div className="top">
+                <div className="item">
+                    <h1>Categories</h1>
+                    <span>Women</span>
+                    <span>Men</span>
+                    <span>Shoes</span>
+                    <span>Accessories</span>
+                    <span>New Arrivals</span>
+                </div>
+                <div className="item">
+                    <h1>Links</h1>
+                    <span>FAQ</span>
+                    <span>Pages</span>
+                    <span>Stores</span>
+                    <span>Compare</span>
+                    <span>Cookies</span>
+                </div>
+                <div className="item">
+                    <h1>Contact</h1>
+                    <span>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut tempore autem dolore. Laboriosam provident doloremque sequi dolorem fugiat, exercitationem nemo dolores et ipsam. Ex nesciunt ratione repudiandae eos officia sunt!
+                    </span>
+                </div>
+                <div className="item">
+                    <h1>About</h1>
+                    <span>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut tempore autem dolore. Laboriosam provident doloremque sequi dolorem fugiat, exercitationem nemo dolores et ipsam. Ex nesciunt ratione repudiandae eos officia sunt!
+                    </span>
+                </div>
+            </div>
 
-      
-    </div>
-  )
+            {/* BOTTOM */}
+            <div className="bottom">
+                <div className="left">
+                    <span className='logo'>Store</span>
+                    <span className='copyright'>
+                        © Copyright 2023. All Rights Reseved
+                    </span>
+                </div>
+                <div className="right">
+                    <img src="img/payment.png" alt="" />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 
