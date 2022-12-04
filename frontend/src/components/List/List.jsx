de0925b2697314705bffa27923c58b789578f0bc
Raj Mandloii <rajmandloi01.01@gmail.com>
2022-12-04 15:19:39 +0530

    product ui added

---
+++ frontend/src/components/List/List.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Card } from '../Card/Card'
+import "./List.scss"
+export const List = () => {
+    const data = [
+        {
+          id: 1,
+          img: "https://images.unsplash.com/photo-1495121605193-b116b5b9c5fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fGZhc2hpb258ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+          img2: "https://images.unsplash.com/photo-1509319117193-57bab727e09d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGZhc2hpb258ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+          title: 'Long sort graphic tshort',
+          isNew: true,
+          oldPrice:19,
+          price:12
+        },
+        {
+          id: 2,
+          img: "https://images.unsplash.com/photo-1495121605193-b116b5b9c5fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fGZhc2hpb258ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+          img2: "https://images.unsplash.com/photo-1509319117193-57bab727e09d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGZhc2hpb258ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+          title: 'Long sort graphic tshort',
+          isNew: true,
+          oldPrice:19,
+          price:12
+        },
+        {
+          id: 3,
+          img: "https://images.unsplash.com/photo-1495121605193-b116b5b9c5fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fGZhc2hpb258ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+          img2: "https://images.unsplash.com/photo-1509319117193-57bab727e09d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGZhc2hpb258ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+          title: 'Long sort graphic tshort',
+          isNew: true,
+          oldPrice:19,
+          price:12
+        },
+        {
+          id: 4,
+          img: "https://images.unsplash.com/photo-1495121605193-b116b5b9c5fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fGZhc2hpb258ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+          img2: "https://images.unsplash.com/photo-1509319117193-57bab727e09d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGZhc2hpb258ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+          title: 'Long sort graphic tshort',
+          isNew: true,
+          oldPrice:19,
+          price:12
+        },
+        {
+          id: 5,
+          img: "https://images.unsplash.com/photo-1495121605193-b116b5b9c5fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fGZhc2hpb258ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+          img2: "https://images.unsplash.com/photo-1509319117193-57bab727e09d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGZhc2hpb258ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+          title: 'Long sort graphic tshort',
+          isNew: true,
+          oldPrice:19,
+          price:12
+        },
+        {
+          id: 6,
+          img: "https://images.unsplash.com/photo-1495121605193-b116b5b9c5fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fGZhc2hpb258ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+          img2: "https://images.unsplash.com/photo-1509319117193-57bab727e09d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGZhc2hpb258ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+          title: 'Long sort graphic tshort',
+          isNew: true,
+          oldPrice:19,
+          price:12
+        },
+    
+      ]
+    return (
+        <div className='list'>{data?.map((el)=>(
+            <Card item={el} key={el.id}/>
+        ))}</div>
+    )
+}
--- frontend/src/pages/Product/Product.jsx
+++ frontend/src/pages/Product/Product.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
-import "./Product.scss"
- const Product = () => {
+import React, { useState } from 'react'
+import "./Product.scss";
+
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import BalanceIcon from "@mui/icons-material/Balance"
+
+const Product = () => {
+  const [selectedImg, setSelectedImg] = useState(0)
+  const [quantity, setQuantity] = useState(1)
+  const images = [
+    "https://images.unsplash.com/photo-1669950200209-69d8292c032f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+    "https://images.unsplash.com/photo-1669950200209-69d8292c032f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+
+  ]
   return (
-    <div>Product</div>
+    <div className='product'>
+      <div className="left">
+        <div className="images">
+          <img src={images[0]} alt=""
+            onClick={e => setSelectedImg(0)}
+          />
+          <img src={images[1]} alt=""
+            onClick={e => setSelectedImg(1)}
+          />
+        </div>
+        <div className="mainImg">
+          <img src={images[selectedImg]} alt="" />
+        </div>
+
+      </div>
+      <div className="right">
+        <h1>Title</h1>
+        <span className='price'>$199</span>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime magnam nostrum vel earum quisquam veritatis porro cumque sit dolore totam esse hic, architecto ipsum molestias ex eaque omnis repellat deleniti?</p>
+        <div className="quantity">
+          <button onClick={() => setQuantity(prev => prev === 1 ? 1 : prev - 1)}>-</button>
+          {quantity}
+          <button onClick={() => setQuantity(prev => prev + 1)}>+</button>
+        </div>
+        <button className='add'>
+          <AddShoppingCartIcon /> ADD TO CART
+        </button>
+
+        <div className="link">
+          <div className="item">
+            <FavoriteBorderIcon /> ADD TO WISH LIST
+          </div>
+          <div className="item">
+            <BalanceIcon /> ADD TO COMPARE
+          </div>
+          <div className="info">
+            <span>Vendor: Polo</span>
+            <span>Product Type: T-Shirt</span>
+            <span>Tag: T-Shirt, Women, Top</span>
+          </div>
+          <hr />
+          <div className="info">
+            <span>DESCRIPTION</span>
+            <hr />
+            <span>ADDITIONAL INFORMATION</span>
+            <hr />
+            <span>FAQ</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 export default Product
